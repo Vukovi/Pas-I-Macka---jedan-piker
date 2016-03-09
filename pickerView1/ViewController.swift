03214775dff2b14f5366845f78ace037cd07181e
Borlja <jelenaborlja@macbook-air.local>
2016-03-09 13:09:01 +0100

    Initial Commit

---
+++ pickerView1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pickerView1
+//
+//  Created by Borlja on 93//16.
+//  Copyright © 2016 Vlada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
